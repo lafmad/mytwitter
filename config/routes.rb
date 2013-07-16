700434033dec09edf98d7a8a39fa44073afba51b
lafmad <inaircastle@gmail.com>
2013-07-16 22:05:14 +0800

    Make basic User model(with secure passwords)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Mytwitter::Application.routes.draw do
   get "static_pages/home"
 
+  resources :users
+  match '/signup',  to: 'users#new'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -50,7 +53,7 @@ Mytwitter::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'static_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
