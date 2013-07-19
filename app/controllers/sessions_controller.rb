b709700124a21a4e142448226c3c834269cf1ca9
lafmad <inaircastle@gmail.com>
2013-07-20 00:08:45 +0800

    unfinish add admin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 		user = User.find_by_email(params[:session][:email].downcase)
 		if user && user.authenticate(params[:session][:password])
 			sign_in(user)
-			redirect_to user_path(user.id)
+			redirect_back_or(user)
 		else
 			flash[:error] = '错误的邮箱或者密码不匹配'
 			render 'new'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 #coding: utf-8
 class UsersController < ApplicationController
+	before_filter :signed_in_user, only: [:index,:edit, :update]
+	before_filter :correct_user,   only: [:edit, :update]
 
 	def show
 		@user = User.find(params[:id])
@@ -19,4 +21,37 @@ class UsersController < ApplicationController
 			render 'new'
 		end
 	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.new(params[:user])
+		if @user.update_attributes(params[:user])
+			flash[:success] = "更新成功"
+			sign_in(@user)
+			redirect_to @user
+		else
+			render 'edit'
+		end
+	end
+
+	def index
+		@users = User.paginate(page: params[:page])
+	end
+
+	private
+	def signed_in_user
+		unless signed_in?
+			store_location
+			redirect_to signin_path,notice:"您还没登录呐!" unless signed_in?
+		end
+	end
+
+	def correct_user
+		@user = User.find(params[:id])
+		redirect_to(root_path) unless current_user?(@user)
+	end
+
 end
