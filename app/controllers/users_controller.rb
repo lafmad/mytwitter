251f1eb847cd1810390c4e0852685384a6b30148
lafmad <inaircastle@gmail.com>
2013-07-20 11:17:36 +0800

    Finish updating-users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@
 class UsersController < ApplicationController
 	before_filter :signed_in_user, only: [:index,:edit, :update]
 	before_filter :correct_user,   only: [:edit, :update]
+	before_filter :admin_user,     only: :destroy
 
 	def show
 		@user = User.find(params[:id])
@@ -41,6 +42,12 @@ class UsersController < ApplicationController
 		@users = User.paginate(page: params[:page])
 	end
 
+	def destroy
+		User.find(params[:id]).destroy
+		flash[:sucess]="敌人已消灭!"
+		redirect_to users_path
+	end
+
 	private
 	def signed_in_user
 		unless signed_in?
@@ -53,5 +60,9 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 		redirect_to(root_path) unless current_user?(@user)
 	end
+
+	def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
 
 end
