4024852f8a6c1e8c71f8c13ab23a9b4ce1333a84
lafmad <inaircastle@gmail.com>
2013-07-20 22:11:01 +0800

    stop to see a movie,smile~

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
 	attr_accessible :email, :name ,:password, :password_confirmation
 	has_secure_password
 	has_many :microposts,dependent: :destroy
+	has_many :relationships,foreign_key:"follower_id",dependent: :destroy
+	has_many :followed_users, through: :relationships, source: :followed
 	
 	before_save {|user| user.email = email.downcase}
 	before_save :create_remember_token
@@ -12,6 +14,20 @@ class User < ActiveRecord::Base
 	validates :email,presence: true,format: {with: VALID_EMAIL_REGEX},uniqueness:{case_sensitive: false}
 	validates :password, presence: true,length: {minimum: 6}
 	validates :password_confirmation,presence: true
+
+	def following?(other_user)
+		relationships.find_by(followed_id: other_user.id)
+	end
+
+	def follow!(other_user)
+		relationships.create!(followed_id: other_user.id)
+	end
+
+	def unfollow!(other_user)
+		relationships.find_by(followed_id: other_user.id).destroy
+	end
+
+
 
 	def create_remember_token
 		self.remember_token = SecureRandom.urlsafe_base64
