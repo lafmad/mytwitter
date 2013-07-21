71645ddaf93ba46b9186739f68c88052e6c6948b
lafmad <inaircastle@gmail.com>
2013-07-21 22:07:52 +0800

    finish now try to find a job

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,13 @@ Mytwitter::Application.routes.draw do
   resources :users
   resources :sessions,only: [:new,:create,:destroy]
   resources :microposts,only:[:create,:destroy]
+  resources :users do
+    member do
+      get :following,:followers
+    end
+  end
+  resources :relationships, only: [:create,:destroy]
+  
   match '/signup',  to: 'users#new'
   match '/signin',  to: 'sessions#new'
   match '/signout', to: 'sessions#destroy',via: :delete
