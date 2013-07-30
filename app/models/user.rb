97ed99bfb144f1d83a59c09e81347660ce67e1dd
lafmad <inaircastle@gmail.com>
2013-07-30 20:59:17 +0800

    add floor

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,10 @@ class User < ActiveRecord::Base
 	has_many :relationships,foreign_key:"follower_id",dependent: :destroy
 	has_many :followed_users, through: :relationships, source: :followed
 
-	  has_many :reverse_relationships, foreign_key: "followed_id",
+	  has_many :same_relationships, foreign_key: "followed_id",
                                    class_name:  "Relationship",
                                    dependent:   :destroy
-  has_many :followers, through: :reverse_relationships, source: :follower
+  has_many :followers, through: :same_relationships, source: :follower
 	
 	before_save {|user| user.email = email.downcase}
 	before_save :create_remember_token
